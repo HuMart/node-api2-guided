29e6c6415ae2c3ec50bd482398afd25351cb2237
HuMart <manya01@protonmail.com>
2020-02-05 16:26:21 -0500

    router working

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const server = require('./api/server.js');
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
+
+
+// create a server.js file somewhere
+// move everything but serve.listen() out of index
+// require the server from index.js
+// module.exports the server from server.js
+// update the path to acommodate the changes (hubs-model.js)
